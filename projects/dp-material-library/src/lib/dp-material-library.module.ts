ea9a65e9525b63a5f1772986229026d10373292e
Dennis Porter Jr <dennisporterjr@gmail.com>
2020-06-26 18:19:04 -0400

    Initial scaffolding

---
+++ projects/dp-material-library/src/lib/dp-material-library.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { TableComponent } from './table/table.component';
+
+
+
+@NgModule({
+  declarations: [
+    TableComponent,
+  ],
+  imports: [
+  ],
+  exports: [
+    TableComponent,
+  ]
+})
+export class DpMaterialLibraryModule { }
---
+++ projects/dp-material-library/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of dp-material-library
+ */
+
+export * from './lib/dp-material-library.module';
+
+export * from './lib/table/table.component';
